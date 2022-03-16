6d1276f9c0b78b124013b44de3f4e423351ef332
nechapay <nechapay@list.ru>
2022-03-16 09:42:30 +0300

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter array rows: ");
         int arrRows = sc.nextInt();
         System.out.print("Enter array cols: ");
         int arrCols = sc.nextInt();
-        int [][] arr = new int[arrRows][arrCols];
-        for(int i = 0; i < arrRows; i++) {
-            for(int j = 0; j < arrCols; j++) {
+        int[][] arr = new int[arrRows][arrCols];
+        for (int i = 0; i < arrRows; i++) {
+            for (int j = 0; j < arrCols; j++) {
                 System.out.print("Enter array item: ");
                 arr[i][j] = sc.nextInt();
             }
         }
+        sc.close();
         System.out.println("You entered an array: ");
-        for(int i = 0; i < arrRows; i++) {
-            for(int j = 0; j < arrCols; j++) {
+        for (int i = 0; i < arrRows; i++) {
+            for (int j = 0; j < arrCols; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
